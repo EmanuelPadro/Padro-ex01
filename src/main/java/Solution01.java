4b1841eafa3301aeb8478b8fb46c405696b547d8
Emanuel <emanuelpadro@icloud.com>
2021-09-05 19:26:12 -0400

    ALL GOOD COMMIT

--- src/main/java/Solution01.java
+++ src/main/java/Solution01.java
@@ -3,8 +3,18 @@
  *  Copyright 2021 first_name last_name
  */
 
+import java.util.Scanner;
+
+/* Will all be located in main, will prompt user for name
+will then take name and put it in a variable and print a
+statement plus variable
+*/
 public class Solution01 {
     public static void main(String[] args) {
-
+        Scanner Input = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String Name =  Input.next();
+        System.out.println("Hello, " + Name + ", nice to meet you!");
     }
+
 }
