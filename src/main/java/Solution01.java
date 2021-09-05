28ed3deb17d3a789474cc20423cae76f08b85d41
Emanuel <emanuelpadro@icloud.com>
2021-09-05 19:03:37 -0400

    Initial commit

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+public class Solution01 {
+    public static void main(String[] args) {
+
+    }
+}
